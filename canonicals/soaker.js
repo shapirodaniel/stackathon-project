ffd06e53d4cd58fa6451f7018b059ba96f26c10a
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-12 14:46:40 -0600

    add soaker.js

---
+++ canonicals/soaker.js
@@ -0,0 +1,23 @@
+const seedSoaker = {
+	flour: [
+		{
+			name: 'whole wheat',
+			bp: 10,
+		},
+		{ name: 'rye', bp: 5 },
+	],
+	dries: [
+		{
+			name: '9 grain seed mix',
+			bp: 100,
+		},
+	],
+	liquids: [
+		{
+			name: 'water',
+			bp: 140,
+		},
+	],
+};
+
+module.exports = { seedSoaker };
