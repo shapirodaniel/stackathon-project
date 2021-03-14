fd30acb75db9b6a30781510390cefff6e853560f
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-14 12:20:53 -0500

    add canonical match fn to compare recipes

--- compareRecipes.js
+++ compareRecipes.js
@@ -1,8 +1,37 @@
 // next compare user recipe to canonicals
 // require the canonicals and sub-classifiers
-const canonicals = require('../canonicalRecipes');
+const canonicals = require('./canonicals/recipes');
 const { getClassifiedRecipe } = require('./recipe-classifiers');
 const { getScores } = require('./production-factors');
+
+// do a rough match on ingredient classes and subclasses by length
+// if that doesn't return 1-2 results, check edit distance of ingredients
+
+const findCanonicalMatch = userInput => {
+	// select a canonical recipe
+	// how to do this?
+	// compare ingredient class total bp first
+	// most recipes will have flour, water, salt, yeast
+	// so we can discard based on the remaining ingredient classes
+	// that will narrow the choices
+	// return the narrowed choices as array to user to select
+	// in react, map these to a button dialogue:
+	/*
+	return (
+		<div>
+			<div>Which recipe matches yours best?</div>
+			canonicalMatches.map(recipe =>
+				(
+					<button
+						type='button'
+						onClick={chooseRecipe()}
+					>{recipe.name}</button>
+				)
+			);
+		</div>
+	);
+	*/
+};
 
 // getRecipeDiff() takes in two objs { pH, yeastMotility, yeastConcentration }, combines their scores, and returns a single totalDiff object
 const getRecipeDiff = (userInput, canonical) => {
